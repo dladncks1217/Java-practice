6d3f8fcab3457238cf511546ed4c0a31cda203e6
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-05-29 17:18:04 +0900

    0529

--- FileReaderTest/src/FileReaderTest.java
+++ FileReaderTest/src/FileReaderTest.java
@@ -6,7 +6,7 @@ public class FileReaderTest {
 		try {
 			FileReader fr = new FileReader("C:\\Users\\dlaxo\\hellocheck.txt");
 			int c;
-			System.out.println("Encoding set:" +fr.getEncoding());
+			System.out.println("Encoding set:" +fr.getEncoding());//인코딩방식(영어가 아닐경우를 말한다) 밑 encoding set 은 인코딩방식을 말한것이다.
 			while ((c = fr.read())!=-1) {
 				System.out.print((char)c);
 			}
