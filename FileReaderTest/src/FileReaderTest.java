fa2ad0dda0f5d75779033ab579ad6ec318a68ada
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-05-29 17:34:32 +0900

    0529

--- FileReaderTest/src/FileReaderTest.java
+++ FileReaderTest/src/FileReaderTest.java
@@ -1,16 +1,20 @@
-import java.io.FileReader;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 public class FileReaderTest {
 
 	public static void main(String[] args) {
 		try {
-			FileReader fr = new FileReader("C:\\Users\\dlaxo\\hellocheck.txt");
+			FileInputStream fis = new FileInputStream("C:\\Users\\dlaxo\\hellocheck.txt");
+			InputStreamReader isr = new InputStreamReader(fis, "MS949");
 			int c;
-			System.out.println("Encoding set:" +fr.getEncoding());//인코딩방식(영어가 아닐경우를 말한다) 밑 encoding set 은 인코딩방식을 말한것이다.
-			while ((c = fr.read())!=-1) {
+			System.out.println("Encoding set:" +isr.getEncoding());//인코딩방식(영어가 아닐경우를 말한다) 밑 encoding set 은 인코딩방식을 말한것이다.
+			while ((c = isr.read())!=-1) {
 				System.out.print((char)c);
 			}
-			fr.close();
+			isr.close();
+			fis.close();
+			
 		}catch(IOException e) {
 			System.out.println("IO Error");
 		}
