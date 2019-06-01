12ed14f8542aa8d5ad46d872dac0d3ea3af9e8df
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-06-01 17:45:43 +0900

    0601

---
+++ Homework_0529/src/Homework_0529.java
@@ -0,0 +1,22 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Homework_0529 {
+
+	public static void main(String[] args) {
+		try {
+			FileInputStream fis = new FileInputStream("C:\\\\Users\\\\dlaxo\\\\eclipse-workspace\\\\Homework_0529\\\\test.txt");
+			InputStreamReader isr = new InputStreamReader(fis, "MS949");
+			int c;
+			System.out.println("Encoding set:" +isr.getEncoding());//인코딩방식(영어가 아닐경우를 말한다) 밑 encoding set 은 인코딩방식을 말한것이다.
+			while ((c = isr.read())!=-1) {
+				System.out.print((char)c);
+			}
+			isr.close();
+			fis.close();
+			
+		}catch(IOException e) {
+			System.out.println("IO Error");
+		}
+	}
+}
