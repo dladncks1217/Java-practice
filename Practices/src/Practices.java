99f96569a72ce0999cdb0d12ea80e33c0caf1889
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-06-06 21:59:29 +0900

    0606

--- Practices/src/Practices.java
+++ Practices/src/Practices.java
@@ -2,11 +2,8 @@ import java.util.Scanner;
 
 public class Practices {
 	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int cards = scan.nextInt();
-		int []
-		for(int i = 0;i<cards;i++) {
-			
-		}
+		Object ob = new Object();
+		int a = 123;
+		System.out.println(ob.toString());
 	}
 }
