2d7eaff97cc733bba5b81ac2b86e18e284a4785e
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-07-11 22:32:21 +0900

    0711

--- Practices/src/Practices.java
+++ Practices/src/Practices.java
@@ -1,27 +1,19 @@
 import java.util.Scanner;
-import java.util.StringTokenizer;
+
+class calculate{
+	
+	public calculate() {
+		
+	}
+}
 
 public class Practices {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int a,b,c,max,min,cen;
-		a = scan.nextInt();
-		b = scan.nextInt();
-		c = scan.nextInt();
-		max = Math.max(a,b);
-		max = Math.max(max, c);
-		min = Math.min(a, b);
-		min = Math.min(min, c);
-		if(a<max&&a>min||max==a||min==a) {
-			System.out.println(a);
-		}
-		else if(b<max&&b>min||max==b||min==b) {
-			System.out.println(b);
-		}
-		else if(c<max&&c>min||max==c||min==c) {
-			System.out.println(c);
-		}
-	
+		String str = scan.next();
+		
+		String[] number = str.split(",");
+		System.out.println(number[0]);
 		
 	}
 }
