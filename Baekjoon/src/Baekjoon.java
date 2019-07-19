01c1dc15019660e32f2b59c246ba3d0e0d4dd23e
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-07-20 00:38:41 +0900

    0720

--- Baekjoon/src/Baekjoon.java
+++ Baekjoon/src/Baekjoon.java
@@ -1,9 +1,27 @@
 import java.util.Scanner;
-public class Baekjoon {
 
+class Circulate{
+	int a;
+	int b;
+	
+	public Circulate(int a,int b) {
+		this.a = a;
+		this.b = b;
+	}
+	public void boo() {
+		System.out.println((a+b)*(a-b));
+		}
+	}
+}
+
+public class Baekjoon {
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		Circulate c = new Circulate(a,b);
+		c.boo();
 		
 		
 	}
-
 }
--- Practices/src/Practices.java
+++ Practices/src/Practices.java
@@ -1,19 +1,29 @@
 import java.util.Scanner;
 
-class calculate{
+class Circulate{
+	int a;
+	int b;
 	
-	public calculate() {
-		
+	public Circulate(int a,int b) {
+		this.a = a;
+		this.b = b;
 	}
+	public void boo() {
+		System.out.println((a+b)*(a-b));
+		
+		}
 }
+
 
 public class Practices {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		String str = scan.next();
-		
-		String[] number = str.split(",");
-		System.out.println(number[0]);
-		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		if(a>=1000||b>=1000) {
+			return;
+		}
+		Circulate c = new Circulate(a,b);
+		c.boo();
 	}
 }
