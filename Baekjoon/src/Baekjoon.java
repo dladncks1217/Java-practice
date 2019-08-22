d3909e0f3fbd0d3858535337ce7cb527fe5ae547
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-08-22 20:05:11 +0900

    0822

--- Baekjoon/src/Baekjoon.java
+++ Baekjoon/src/Baekjoon.java
@@ -1,34 +1,15 @@
+import java.util.Arrays;
 import java.util.Scanner;
 public class Baekjoon {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int result = 0;
-		if(a%b==0) {
-			System.out.println("00");
-			return;
-		}
-		else {
-			result = a-(a%b);
-		}
-		
-		result = result%1000;
-		result = result%100;
-		
-		while(true) {
-			result = result-b;
-			if(result<10&&result>0) {
-				System.out.println("0"+result);
-				return;
-			}
-			else {
-				System.out.println(result);
-				return;
+		char [][]board = new char[8][8];
+		for(int i = 0;i<8;i++) {
+			for(int j = 0;j<8;j++) {
+				board[i][j] = scan.next().charAt(0);
 			}
 		}
+		System.out.println(board[7][7]);
 	}
-	
-
 }
