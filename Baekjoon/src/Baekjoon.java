a962227a710641307c95cc79b496d06d71ecc8f4
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-11-12 23:48:39 +0900

    1112

--- Baekjoon/src/Baekjoon.java
+++ Baekjoon/src/Baekjoon.java
@@ -5,14 +5,9 @@ public class Baekjoon {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int a = scan.nextInt();
-		int []arr = new int[a];
-		for(int i = 0;i<a;i++) {
-			arr[i] = scan.nextInt();
+		for(int i = 1;i<=a;i++) {
+			String b = scan.next().trim(" ");
+			System.out.println(i+"."+" "+b);
 		}
-		Arrays.sort(arr);
-		for(int i = a-1;i>=0;i--) {
-			System.out.println(arr[i]);
-		}
-		
 	}
 }
--- Practices/src/Practices.java
+++ Practices/src/Practices.java
@@ -1,12 +1,29 @@
+import java.util.Scanner;
 public class Practices {
-	public static void main(String[] args) {
-		int a = 2;
-		int i;
-		for(a = 2;a<10;a++) {
-			for(i = 2;i<10;i++) {
-				System.out.print(a+" x "+i+" = "+i*a+"<br>");
-			}
-			System.out.println();
-		}
-	}
+   String title, author;
+   public Practices(String title, String author){
+      this.title=title;
+      this.author=author;
+   }
+   
+   public static void main(String [] args) {
+      Practices [] booklist = new Practices[2];
+      Scanner scanner = new Scanner(System.in);
+      for (int i=0;i<2;i++) {
+         System.out.println("제목:");
+         String title = scanner.nextLine();
+         System.out.println(title);
+         System.out.println("저자:");
+         String author = scanner.nextLine();
+         System.out.println(author);
+         booklist[i] = new Practices(title,author);
+      }
+      for (int i=0;i<2;i++) {
+         System.out.print("("+booklist[i].title+","+booklist[i].author+")");
+      }
+      scanner.close();
+      
+      
+   }
+
 }
